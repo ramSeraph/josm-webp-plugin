496f340e249ce371a61720c1db0a770425c1d6b8
ramSeraph <kandimalla.sreeram@gmail.com>
2022-05-19 16:33:08 +0530

    First attempt

---
+++ lib/src/main/java/org/openstreetmap/josm/plugins/webp/WebpPlugin.java
@@ -0,0 +1,15 @@
+package org.openstreetmap.josm.plugins.webp;
+
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+
+public class WebpPlugin extends Plugin {
+  /**
+   * Will be invoked by JOSM to bootstrap the plugin
+   *
+   * @param info  information about the plugin and its local installation
+   */
+  public WebpPlugin(PluginInformation info) {
+     super(info);
+  }
+}
